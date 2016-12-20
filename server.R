505c393e91fc1acff13eafc885aaecbb8ee06b91
Kriti Sen Sharma <ksen@paradigm4.com>
2016-12-20 16:41:09 +0000

    added implementation of multicolumn comparison

--- server.R
+++ server.R
@@ -8,40 +8,48 @@ dyBarChart <- function(dygraph) {
             path = system.file("examples/plotters/barchart.js",
                                package = "dygraphs"))
 }
+dyMultiColumn <- function(dygraph) {
+  dyPlotter(dygraph = dygraph,
+            name = "MultiColumn",
+            path = system.file("examples/plotters/multicolumn.js",
+                               package = "dygraphs"))
+}
 shinyServer(function(input, output) {
   
   observeEvent(input$chooseSecondArray, {
     if (input$chooseSecondArray == FALSE) {shinyjs::disable("array2") } else {shinyjs::enable("array2") }
   })
 
-  get_array_stats_ux <- reactive({
+  get_array_stats_array1 <- reactive({
     stats1 = get_array_stats(input$array1, input$useCache)
     if (min(stats1$count) == 0) {factor=1} else {factor= min(stats1$count)}
     stats1$scaledCount = stats1$count / factor
-    
-    stats1$arr = input$array1
-    statsC = stats1
-    
-    if (input$chooseSecondArray){
-      #stats2 = data.frame(inst=c(1:64), count=as.integer(runif(64)*100)) 
-      # stats2 = iquery(sprintf("project(summarize(%s, 'per_instance=1'), count)", input$array2), return = TRUE)
-      stats2 = get_array_stats(input$array2, input$useCache)
-      if (min(stats2$count) == 0) {factor2 = 1} else {factor2= min(stats2$count)}
-      stats2$scaledCount = stats2$count / factor2
-      
-      stats2$arr = input$array2
-      statsC = rbind(stats1, stats2)
-    }  
-    statsC = statsC[, c("inst", "count")]
-    return(statsC)
+    return(stats1[, c("inst", "count")])
   })
 
+  get_array_stats_array2 <- reactive({
+    stats2 = get_array_stats(input$array2, input$useCache)
+    if (min(stats2$count) == 0) {factor2 = 1} else {factor2= min(stats2$count)}
+    stats2$scaledCount = stats2$count / factor2
+    
+    return(stats2[, c("inst", "count")])
+  })
+  
   output$dygraph <- renderDygraph({
-    stats = get_array_stats_ux()
-    dygraph(stats, main = "Count / Instances") %>%
-      dyAxis("y", label = "Count", valueRange = c(-0.05*max(stats$count), max(stats$count)))%>%
-      dySeries("count", label = input$array1)  %>%
-      dyBarChart()
+    stats1 = get_array_stats_array1()
+    if (!input$chooseSecondArray) {
+      dygraph(stats1, main = "Count / Instances") %>%
+        dyAxis("y", label = "Count", valueRange = c(-0.05*max(stats1$count), max(stats1$count)))%>%
+        dySeries("count", label = input$array1)  %>%
+        dyBarChart()
+    } else {
+      stats2 = get_array_stats_array2()
+      statsC <- data.frame(inst=stats1$inst, stats1=stats1$count, stats2=stats2$count)
+      colnames(statsC)=c("inst", input$array1, input$array2)
+      dygraph(statsC) %>%
+        dyAxis("y", label = "Count", valueRange = c(-0.05*max(stats1$count), max(stats1$count)))%>%
+        dyMultiColumn()
+    }
   })
   
   output$summary <- renderPrint({
--- ui.R
+++ ui.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(dygraphs)
 tryCatch({
   library(scidb)
   scidbconnect()
